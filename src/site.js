cf39d18ff13a325c5e933fa4c0468ca770c6b0c2
idockter <idockter@iu.edu>
2024-01-11 16:06:34 -0500

    cleaning up code and folder

---
+++ src/site.js
@@ -0,0 +1,25 @@
+// NAVBAR interactions (mobile view w/dropdown)
+const navLinks = document.querySelector('.nav-links');
+function onToggleMenu(e) {
+    e.name = e.name === 'menu' ? 'close' : 'menu';
+    navLinks.classList.toggle('top-[35%]');
+    navLinks.classList.toggle('hidden');
+
+    console.log(navLinks);
+}
+
+
+// TAB LINKS for skills, experience, education
+var tabLinks = document.getElementsByClassName('tab-links');
+var tabContents = document.getElementsByClassName('tab-contents');
+
+function openTab(tabName) {
+    for (tabLink of tabLinks) {
+        tabLink.classList.remove('active-link');
+    }
+    for (tabContent of tabContents) {
+        tabContent.classList.remove('active-tab');
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabName).classList.add('active-tab');
+}
